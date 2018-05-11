31b9570913f8bb5b3ca577f30a531ba863761cd8
Zoe Williams <mwilliamszoe@gmail.com>
2018-05-10 19:02:54 -0500

    Add small amount of JS.

---
+++ js-calc.js
@@ -0,0 +1,18 @@
+// Make the operators appear in the empty box when clicked
+// eventlistner
+// document.getElementById("answer").addEventListener("click", myFunction);
+                
+// function myFunction() {
+//     document.getElementById("answer").innerHTML = "YOU CLICKED ME!";
+// }
+
+// onclick
+// document.getElementById("answer").onclick = function() {myFunction()};
+
+// function myFunction() {
+//     document.getElementById("answer").innerHTML = "YOU CLICKED ME!";
+// }
+
+ function operators() {
+    document.getElementById("divide").innerHTML = "divide";
+}
