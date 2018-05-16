1e776e09896b9f70690decf6d82dec1db48692e2
Zoe Williams <mwilliamszoe@gmail.com>
2018-05-16 14:32:07 -0500

    If/Else statments instead of original code.

--- js-calc.js
+++ js-calc.js
@@ -2,7 +2,7 @@
 
 let keys = document.querySelectorAll('.keys')
 let answerBox = document.querySelector('#answer-box')
-// let input = []
+let input = []
 // let answer = ''
 
 
@@ -11,22 +11,46 @@ for (i=0; i<keys.length; i++) {
     keys[i].addEventListener('click', function (event) {
         let  btnClk = event.target;
         // console.log(btnClk.innerHTML);
-        let numb = document.getElementById("answer-box").innerHTML = btnClk.innerHTML;
-        let input = [numb, "numb"];
-        console.log(input);
-        
+
+        const keyInner = btnClk.innerHTML;
+            if ( keyInner === "C") {
+                return input = [];
+                // console.log(input)
+                // console.log("test C")
+            } else {
+                // const keyNumbers = parseInt(keyInner)
+                // input.push(keyNumbers)
+                // console.log(input)
+                // answerBox.textContent = keyNumbers;
+                // console.log(answerBox)
+                
+                const keyNumbers = parseInt(keyInner)
+                input.push(keyNumbers)
+                console.log(input)
+                answerBox.textContent = keyNumbers;
+                console.log(answerBox)
+            }
+
     } )
 }
 
 
+if (keyInner === "+" 
+    (keyInner === "-"
+    (keyInner === "/"
+    (keyInner === "x"
+    {
 
 
 
-
-
-// .join for arrays
+// .join to get rid of commas
 // eval() method to evaluate operations
 // parseInt()
 //  with if statements - start with operators first
 // use else statements for numbers
 // .textContent
+
+// get rid of commas with join
+// find a way to turn make parseInt ignore operators
+// eval entire statement with = sign
+ 
